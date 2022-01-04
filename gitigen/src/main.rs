77068e9947c2b02bdfc17cb5d7caa23cfdb74ed4
J7314b <dixinlily@hotmail.com>
2022-01-04 14:28:05 -0500

    Implemented writing to gitignore functionality

--- gitigen/src/main.rs
+++ gitigen/src/main.rs
@@ -1,4 +1,20 @@
 use std::fs::File;
+use std::io::Write;
+
+const TEMPLATE: &str = r#"
+# This is a general gitignore template
+debug*/
+target*/
+build*/
+release*/
+
+# Operating system generated files
+.DS_Store
+*.temp
+*.cache
+*.pdb
+*.tmp
+"#;
 
 fn warn_gitignore_exists() -> Result<(), i32> {
     println!("⛔😱 Sorry, but a `.gitignore` already exists in this folder!");
@@ -8,15 +24,25 @@ fn warn_gitignore_exists() -> Result<(), i32> {
 fn create_gitignore() -> Result<(), i32> {
     let f = File::create(".gitignore");
     match f {
-        Ok(_) => {
-            println!("✨ 🍰 ✨ Created your gitignore!");
-            Ok(())
+        Ok(mut f) => {
+            let w = write!(&mut f, "{}", TEMPLATE);
+            match w {
+                Ok(_) => {
+                    println!("✨ 🍰 ✨ Created your gitignore!");
+                    Ok(())
+                },
+                Err(_) => {
+                    println!("⛔😱 Oh no! `.gitignore` could not be created!");
+                    Err(1)
+                }
+            }
         },
         Err(_) => {
             println!("⛔😱 Oh no! `.gitignore` could not be created!");
             Err(1)
         }
     }
+
 }
 
 fn main() -> Result<(), i32> {
