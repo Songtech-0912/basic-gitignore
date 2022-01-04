1d159248b706222901a7a22c48e62364cdf5e420
J7314b <dixinlily@hotmail.com>
2022-01-04 13:52:56 -0500

    Create gitigen in Rust

--- gitigen/src/main.rs
+++ gitigen/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+fn warn_gitignore_exists() -> Result<(), i32> {
+    println!("⛔😱 Sorry, but a `.gitignore` already exists in this folder!");
+    Err(1)
+}
+
+fn create_gitignore() -> Result<(), i32> {
+    let f = File::create(".gitignore");
+    match f {
+        Ok(_) => {
+            println!("✨ 🍰 ✨ Created your gitignore!");
+            Ok(())
+        },
+        Err(_) => {
+            println!("⛔😱 Oh no! `.gitignore` could not be created!");
+            Err(1)
+        }
+    }
+}
+
+fn main() -> Result<(), i32> {
+    let f = File::open(".gitignore");
+    match f {
+        Ok(_) => warn_gitignore_exists(),
+        Err(_) => create_gitignore(),
+    }
 }
